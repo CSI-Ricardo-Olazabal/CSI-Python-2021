f437b064820d29e071931caeee7e956504fe3623
CSI-Ricardo-Olazabal <89258272+CSI-Ricardo-Olazabal@users.noreply.github.com>
2021-10-08 09:38:38 -0400

    Updated rock paper scissors code
    made it more efficient and harder since you dont know what the computer chose untill after youve chosen.

--- Modules/Module5/RockPaperScissors.py
+++ Modules/Module5/RockPaperScissors.py
@@ -1,13 +1,17 @@
 import random
 def RPS():
     moves = ['rock','paper','scissors']
+    wmoves = ['paper','scissors','rock']
     randChoice = random.choice(moves)
-    print(f'Computer selected: {randChoice}')
     yamove = str(input('Your move is? (rock, paper, or scissors) = '))
-    if (randChoice == 'rock' and yamove == 'paper') or (randChoice == 'paper' and yamove == 'scissors') or (randChoice == 'scissors' and yamove == 'rock'):
+    print(f'Computer selected: {randChoice}')
+    if yamove == wmoves[moves.index(randChoice)]:
         print('You won.')
     else:
-        print('You lost.')
+        if yamove == randChoice:
+            print('Tied.')
+        else:
+            print('You lost.')
     keep = str(input('Keep playing? (y or n) = '))
     if keep == 'y':
         RPS()
