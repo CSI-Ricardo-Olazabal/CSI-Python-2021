00ace38fe2e9fc1a8ab4d984c02cc3680569c5bb
CSI-Ricardo-Olazabal <89258272+CSI-Ricardo-Olazabal@users.noreply.github.com>
2021-11-17 20:56:28 -0400

    Projectile Motion JSON
    woot

--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,13 +1,37 @@
 import math
-def ProjectileFunction(experimentalData: experimentalData):
-    time = math.sqrt((2*experimentalData.height)/experimentalData.gravity)
+import os
+from pathlib import Path
+from hello import ExperimentalData
+import json
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+g_ms2 = [3.7, 8.87, 9.81, 3.711, 24.79, 10.44, 8.69, 11.15]
+def projectileFunction(experimentalData: ExperimentalData):
+    planet = experimentalData.planet
+    time = math.sqrt((2*experimentalData.height)/(g_ms2[planets.index(planet)]*3.28084))
     distance = experimentalData.pspeed*time
     print(f"The gun selected for this experiment was a(n) {experimentalData.gun} with ammo {experimentalData.ammo}. The caliber is {experimentalData.caliber}.")
     print(f"Time: {time} seconds.")
     print(f"Distance: {distance} feet.")
-    print(f"Given the speed of the projectile ({experimentalData.pspeed} feet/second) and the height of the {experimentalData.building} ({experimentalData.height} feet), \nwe can calculate the time: {experimentalData.time} seconds, and now, using the time and speed, \nwe can calculate distance: {experimentalData.distance} feet.")
-#ProjectileFunction("FN GL40","40x46 mm","M441(HE) grenade",76,541,"One World Trade Center",10)
+    print(f"Given the speed of the projectile ({experimentalData.pspeed} feet/second), the force of gravity on {planet} ({g_ms2[planets.index(planet)]*3.28084} ft/s^2) and, the height of the {experimentalData.building} ({experimentalData.height} feet), \nwe can calculate the time to be {time} seconds, and now, using the time and speed, \nwe can calculate distance to be {distance} feet.")
+#experimentalData = ExperimentalData("FN GL40","40x46 mm","M441(HE) grenade",76,541,"One World Trade Center",10)
+myDataSet = [
+ExperimentalData("FN GL40","40x46 mm","M441(HE) grenade",76,541,"One World Trade Center","Earth"),
+ExperimentalData("FN GL40","40x46 mm","M441(HE) grenade",76,830,"Burj Khalifa","Venus"),
+ExperimentalData("FN GL40","40x46 mm","M576 (MP-APERS) grenade",269,541,"One World Trade Center","Mars"),
+ExperimentalData("FN GL40","40x46 mm","M576 (MP-APERS) grenade",269,830,"Burj Khalifa","Jupiter"),
+ExperimentalData("NSV \"Utes\"","12.7x108mm","mm BZT-44M gzh",818,830,"Burj Khalifa","Neptune")
+]
+for data in myDataSet:
+    print("\n-----------------------------------------------------------\n")
+    projectileFunction(data)
 
+#serialization
+myOutputPath = Path(__file__).parents[0]
+myOutputFilePath = os.path.join(myOutputPath, 'Projectile-Motion.json')
+print(myOutputFilePath)
+
+with open(myOutputFilePath, 'w') as outfile:
+    json.dump([data.__dict__ for data in myDataSet], outfile)
 #name = "FN GL40"
 #calibur = "40x46 mm"
 #ammo = "M441(HE) grenade"
---
+++ Projects/Projectile-Motion/hello.py
@@ -0,0 +1,9 @@
+class ExperimentalData:
+    def __init__(self,gun:str,caliber:str,ammo:str,pspeed:float,height:float,building:str,planet:str):
+        self.gun = gun
+        self.caliber = caliber
+        self.ammo = ammo
+        self.pspeed = pspeed
+        self.height = height
+        self.building = building
+        self.planet = planet
