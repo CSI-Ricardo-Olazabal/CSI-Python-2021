8990821250513a8106f9bab4d48478ae39452040
CSI-Ricardo-Olazabal <89258272+CSI-Ricardo-Olazabal@users.noreply.github.com>
2022-02-11 09:21:02 -0400

    snek
    snek

--- Projects/Ricardo-Olazabal-SnakeGame.py
+++ Projects/Ricardo-Olazabal-SnakeGame.py
@@ -11,10 +11,36 @@ dis = pygame.display.set_mode((400,300))
 pygame.display.update()
 pygame.display.set_caption("Snake game by JD")
 game_over = False
+x1 = 150
+y1 = 150
+
+x1_change = 0
+y1_change = 0
+
+
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_chsnge = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                x1_change = 0
+                y1_change = -10
+            elif event.key == pygame.K_DOWN:
+                x1_change = 0
+                y1_change = 10
 
+    x1+=x1_change
+    y1+=y1_change
+    dis.fill(black)
+    pygame.draw.rect(dis, yellow, [x1, y1, 10, 10])
+    pygame.display.update
 pygame.quit()
 quit()
+
