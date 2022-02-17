5c99fea32f4c815c5684227644ef4ef579b25bed
CSI-Ricardo-Olazabal <89258272+CSI-Ricardo-Olazabal@users.noreply.github.com>
2022-02-17 09:16:51 -0400

    snake
    snake

--- Projects/Ricardo-Olazabal-SnakeGame.py
+++ Projects/Ricardo-Olazabal-SnakeGame.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 pygame.init()
 
 yellow = (255, 255, 102)
@@ -7,16 +8,31 @@ red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
-dis = pygame.display.set_mode((400,300))
+dis_width = 400
+dis_height = 300
+dis = pygame.display.set_mode((dis_width,dis_height))
+
 pygame.display.update()
-pygame.display.set_caption("Snake game by JD")
+pygame.display.set_caption("Snake game by ME, RICARDO OLAZABAL MUAHAHAHAHAHAHAHA")
+
 game_over = False
-x1 = 150
-y1 = 150
+
+x1 = dis_width/2
+y1 = dis_height/2
 
 x1_change = 0
 y1_change = 0
 
+snake_block = 10
+snake_speed = 30
+
+clock = pygame.time.Clock()
+
+font_style = pygame.font.SysFont(None, 50)
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [110, dis_height/2])
 
 while not game_over:
     for event in pygame.event.get():
@@ -24,23 +40,31 @@ while not game_over:
             game_over = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x1_change = -10
+                x1_change = -1
                 y1_chsnge = 0
             elif event.key == pygame.K_RIGHT:
-                x1_change = 10
+                x1_change = 1
                 y1_change = 0
-            elif event.key == pygame.K_UP:
-                x1_change = 0
-                y1_change = -10
             elif event.key == pygame.K_DOWN:
                 x1_change = 0
-                y1_change = 10
+                y1_change = 1
+            elif event.key == pygame.K_UP:
+                x1_change = 0
+                y1_change = -1
+    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+        game_over = True
 
-    x1+=x1_change
-    y1+=y1_change
+    x1 += x1_change*snake_block
+    y1 += y1_change*snake_block
     dis.fill(black)
-    pygame.draw.rect(dis, yellow, [x1, y1, 10, 10])
-    pygame.display.update
+    pygame.draw.rect(dis, blue, [x1, y1, snake_block, snake_block])
+    pygame.display.update()
+
+    clock.tick(snake_speed)
+
+message("You LOST", red)
+pygame.display.update()
+time.sleep(2)
 pygame.quit()
 quit()
 
