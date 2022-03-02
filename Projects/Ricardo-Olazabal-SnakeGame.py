64bf9bee753d9c3413a4154704d2a5e99b494335
CSI-Ricardo-Olazabal <89258272+CSI-Ricardo-Olazabal@users.noreply.github.com>
2022-03-01 23:05:40 -0400

    snake
    snek

--- Projects/Ricardo-Olazabal-SnakeGame.py
+++ Projects/Ricardo-Olazabal-SnakeGame.py
@@ -23,6 +23,11 @@ snake_speed = 30
 clock = pygame.time.Clock()
 
 font_style = pygame.font.SysFont(None, 25)
+score_font = pygame.font.SysFont(None, 25)
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, blue, [x[0], x[1], snake_block, snake_block])
 
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
@@ -78,11 +83,28 @@ def gameRestart():
 
         x1 += x1_change*snake_block
         y1 += y1_change*snake_block
+        snake_List = []
+        length_of_snake = 1
         dis.fill(black)
         pygame.draw.rect(dis, yellow, [foodx, foody, snake_block, snake_block])
-        pygame.draw.rect(dis, blue, [x1, y1, snake_block, snake_block])
+        #pygame.draw.rect(dis, blue, [x1, y1, snake_block, snake_block])
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > length_of_snake:
+            del snake_List[0]
+        for x in snake_List[: -1]:
+            if x == snake_Head:
+                game_close = True
+        our_snake(snake_block, snake_List) 
+
         pygame.display.update()
 
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0,dis_width-snake_block)/snake_block)*snake_block
+            foody = round(random.randrange(0,dis_width-snake_block)/snake_block)*snake_block
+            length_of_snake += 1
         clock.tick(snake_speed)
 
     pygame.quit()
