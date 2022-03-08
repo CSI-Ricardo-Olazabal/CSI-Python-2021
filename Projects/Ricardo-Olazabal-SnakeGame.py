167aa6011116a8ab7d110d03b462abd898e2bcad
CSI-Ricardo-Olazabal <89258272+CSI-Ricardo-Olazabal@users.noreply.github.com>
2022-03-08 08:53:17 -0400

    finished snake
    finished snake

--- Projects/Ricardo-Olazabal-SnakeGame.py
+++ Projects/Ricardo-Olazabal-SnakeGame.py
@@ -25,6 +25,10 @@ clock = pygame.time.Clock()
 font_style = pygame.font.SysFont(None, 25)
 score_font = pygame.font.SysFont(None, 25)
 
+def My_score(score):
+    value = score_font.render("Your Score: "+str(score), True, blue)
+    dis.blit(value, [0, 0])
+
 def our_snake(snake_block, snake_list):
     for x in snake_list:
         pygame.draw.rect(dis, blue, [x[0], x[1], snake_block, snake_block])
@@ -36,19 +40,21 @@ def message(msg, color):
 def gameRestart():
     game_over = False
     game_close = False
-    
+
     x1 = dis_width/2
     y1 = dis_height/2
 
     x1_change = 0
     y1_change = 0
 
-    foodx = round(random.randrange(0,dis_width-snake_block)/snake_block)*snake_block
-    foody = round(random.randrange(0,dis_width-snake_block)/snake_block)*snake_block
+    snake_List = []
+    length_of_snake = 1
 
+    foodx = round(random.randrange(0,dis_width-snake_block)/10)*10
+    foody = round(random.randrange(0,dis_height-snake_block)/10)*10
 
     while not game_over:
-
+    
         while game_close:
             dis.fill(black)
             message("You LOST Press Q-Quit or P-Play Again.", red)
@@ -68,7 +74,7 @@ def gameRestart():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x1_change = -1
-                    y1_chsnge = 0
+                    y1_change = 0
                 elif event.key == pygame.K_RIGHT:
                     x1_change = 1
                     y1_change = 0
@@ -81,10 +87,9 @@ def gameRestart():
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
             game_close = True
 
-        x1 += x1_change*snake_block
-        y1 += y1_change*snake_block
-        snake_List = []
-        length_of_snake = 1
+        x1 += x1_change*10
+        y1 += y1_change*10
+
         dis.fill(black)
         pygame.draw.rect(dis, yellow, [foodx, foody, snake_block, snake_block])
         #pygame.draw.rect(dis, blue, [x1, y1, snake_block, snake_block])
@@ -99,12 +104,15 @@ def gameRestart():
                 game_close = True
         our_snake(snake_block, snake_List) 
 
-        pygame.display.update()
+        My_score(length_of_snake-1)
 
+        pygame.display.update()
+        
         if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0,dis_width-snake_block)/snake_block)*snake_block
-            foody = round(random.randrange(0,dis_width-snake_block)/snake_block)*snake_block
+            foodx = round(random.randrange(0,dis_width-snake_block)/10)*10
+            foody = round(random.randrange(0,dis_height-snake_block)/10)*10
             length_of_snake += 1
+        
         clock.tick(snake_speed)
 
     pygame.quit()
